0eb1621e5aae8d455d079504154b7a6b3ad60c0d
Viktoria Pundy <e01633403@student.tuwien.ac.at>
2021-04-02 19:30:25 +0200

    Prettified code

--- src/app/graphs/graph.inputParser.ts
+++ src/app/graphs/graph.inputParser.ts
@@ -16,7 +16,7 @@ export default class GraphInputParser {
   parseToInputGraph(): InputGraph {
     let inputGraph: InputGraph = new InputGraph();
     let stations: Station[] = [];
-    let stationsById: { [id: string]: Station } = {};
+    let stationsByID: { [id: string]: Station } = {};
     this.stops.forEach(stop => {
       let station = new Station();
       station.stationName = stop["stop_name"];
@@ -24,51 +24,60 @@ export default class GraphInputParser {
       station.latitude = stop["stop_lat"];
       station.longitude = stop["stop_lon"];
       stations.push(station);
-      stationsById[stop["stop_id"]] = station;
+      stationsByID[stop["stop_id"]] = station;
     });
     inputGraph.nodes = stations;
 
-    let routeIdByTripId: { [id: string]: string } = {};
-    this.trips.forEach(trip => routeIdByTripId[trip["trip_id"]] = trip["route_id"]);
+    let routeIDsByTripIDs: { [id: string]: string } = {};
+    this.trips.forEach(trip => routeIDsByTripIDs[trip["trip_id"]] = trip["route_id"]);
 
-    let routeNameByRouteId: { [id: string]: string } = {};
-    this.routes.forEach(route => routeNameByRouteId[route["route_id"]] = route["route_short_name"]);
+    let routeNamesByRouteID: { [id: string]: string } = {};
+    this.routes.forEach(route => routeNamesByRouteID[route["route_id"]] = route["route_short_name"]);
 
-    let tripsById: { [id: string]: Trip } = {};
+    let tripsByID: { [id: string]: Trip } = {};
     this.stopTimes.forEach(stopTime => {
       let tripID = stopTime["trip_id"];
-      let trip = (tripsById[tripID] == undefined) ? new Trip(tripID) : tripsById[tripID];
+
+      let trip = (tripsByID[tripID] == undefined) ? new Trip(tripID) : tripsByID[tripID];
       trip.addStop(stopTime["stop_id"], stopTime["stop_sequence"]);
-      tripsById[tripID] = trip;
+
+      tripsByID[tripID] = trip;
     });
 
-    let allRouteKeys = Object.keys(tripsById);
-    allRouteKeys.forEach(key => {
-      let obj = tripsById[key];
-      obj.routeID = routeIdByTripId[obj.tripID];
-      obj.routeShortName = routeNameByRouteId[obj.routeID];
-      tripsById[key] = obj;
+    let allTripIDs = Object.keys(tripsByID);
+    allTripIDs.forEach(tripID => {
+      let trip = tripsByID[tripID];
+      trip.routeID = routeIDsByTripIDs[trip.tripID];
+      trip.routeShortName = routeNamesByRouteID[trip.routeID];
+
+      tripsByID[tripID] = trip;
     });
 
-    let largestTripByName = new Map();
-    Object.values(tripsById).forEach(trip => {
-      if (largestTripByName.has(trip.routeShortName)) {
-        let currentLargestTrip = largestTripByName.get(trip.routeShortName);
-        largestTripByName.set(trip.routeShortName, getLargerTrip(currentLargestTrip, trip));
+
+    /**
+     * Searches through all trips for the largest (number of stations) trip for a certain route name
+     */
+    let largestTripByRouteName = new Map();
+    Object.values(tripsByID).forEach(trip => {
+      let routeName = trip.routeShortName;
+      if (largestTripByRouteName.has(routeName)) {
+        let currentLargestTrip = largestTripByRouteName.get(routeName);
+        largestTripByRouteName.set(routeName, getLargerTrip(currentLargestTrip, trip));
       } else {
-        largestTripByName.set(trip.routeShortName, trip);
+        largestTripByRouteName.set(routeName, trip);
       }
     });
 
     let edges: InputEdge[] = [];
-    largestTripByName.forEach((trip: Trip, lineName: string) => {
+    largestTripByRouteName.forEach((trip: Trip, routeName: string) => {
       let stopSequence = trip.stops;
       for (let i = 2; i < stopSequence.size + 1; i++) {
-        let inputEdge = new InputEdge(lineName);
         let id1 = stopSequence.get("" + (i - 1));
         let id2 = stopSequence.get("" + i);
-        inputEdge.station1 = stationsById[id1];
-        inputEdge.station2 = stationsById[id2];
+
+        let inputEdge = new InputEdge(routeName);
+        inputEdge.station1 = stationsByID[id1];
+        inputEdge.station2 = stationsByID[id2];
         edges.push(inputEdge);
       }
     });
